83e9df281354809a4b212c82103fd84eca0db8c1
JinaboCV <jinabocv@gmail.com>
2025-06-07 11:06:13 +0100

    blog section has been removed

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,26 +1,21 @@
 from django.shortcuts import render
-from .models import SkillCategory, Skill, Service, Achievements, BlogPost, ServiceCategory, Experience, Task
+from .models import SkillCategory, Skill, Service, Achievements, ServiceCategory, Experience, Task
 
 def index(request):
     skill_category = SkillCategory.objects.all()
     skills = Skill.objects.all()
     service_category = ServiceCategory.objects.all()
-    
+    services = Service.objects.all()
     experiences = Experience.objects.all()
     tasks = Task.objects.all()
     context = {
         'skill_category': skill_category,
         'skills': skills,
         'service_category' : service_category,
+        'services': services,
         'experiences': experiences,
         'tasks': tasks
         }
     return render(request, 'my_portfolio/index.html', context=context)
 
-def blogs(request):
-    
-    return render(request, 'my_portfolio/blog.html')
-
-def blog_post(request):
-    return render(request, 'my_portfolio/blog_post.html')
 # Create your views here.
